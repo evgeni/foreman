aff84b5643e0ad8609318d0140bae7296e4d2e82
Evgeni Golov <evgeni@golov.de>
2020-10-28 12:53:02 +0100

    use fixed f_ansible

--- bundler.d/plugins.rb
+++ bundler.d/plugins.rb
@@ -1,3 +1,3 @@
-gem 'foreman_ansible', '~> 5.1.0'
+gem 'foreman_ansible', :git => 'https://github.com/evgeni/foreman_ansible.git', :branch => '5.1-stable'
 gem 'foreman_remote_execution', '~> 3.3.0'
 gem 'foreman-tasks', '~> 2.0.0'
